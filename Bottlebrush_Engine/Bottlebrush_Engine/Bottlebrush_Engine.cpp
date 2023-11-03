77346ef87c8181c50174bc27b651adcf8aacdba8
Niamh Wilson <niamhlwilson@gmail.com>
2023-11-03 18:16:09 +0800

    Created Visual Studio cmake project

---
+++ Bottlebrush_Engine/Bottlebrush_Engine/Bottlebrush_Engine.cpp
@@ -0,0 +1,12 @@
+﻿// Bottlebrush_Engine.cpp : Defines the entry point for the application.
+//
+
+#include "Bottlebrush_Engine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
