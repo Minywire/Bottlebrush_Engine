b2a7214b9884a22ff75de292d2cc0ac8bf74f520
Marco Garzon <77040582+The-Spaghetti-Is-Ready@users.noreply.github.com>
2024-02-10 03:44:10 +0800

    Added initial imGUI implementation + Testing
    (currently broken) imGUI has been added to the repo and CMake projects. The only issue now is that an imGUI header in the backend folder can't find the GLFW headers so currently getting compile-time errors. A potential fix to this would be to turn the imGUI backend folder into its own library and maybe give it GLFW as a target link library.

---
+++ Bottlebrush_Engine/Bottlebrush_Engine/BBGUI/BBGUI.cpp
@@ -0,0 +1,47 @@
+//
+// Created by Marco Garzon Lara on 10/02/24.
+//
+
+#include "BBGUI.h"
+
+void initBBGUI(GLFWwindow *targetWindow)
+{
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // IF using Docking Branch
+
+// Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(targetWindow, true);          // Second param install_callback=true will install GLFW callbacks and chain to existing ones.
+    ImGui_ImplOpenGL3_Init();
+}
+
+void updateBBGUIFrameStart()
+{
+    // (Code calls glfwPollEvents() before this)
+    // ...
+    // Start the Dear ImGui frame
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+    ImGui::ShowDemoWindow(); // Show demo window
+}
+
+void updateBBGUIFrameEnd()
+{
+    // Rendering
+    // (Code clears framebuffer, renders other stuff etc. before this)
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    // (Code calls glfwSwapBuffers() etc. after this point)
+}
+
+void shutDownBBGUI()
+{
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+}
