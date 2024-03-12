1511322fac43fe8e9b1d079bae44c8634ca72895
Marco Garzon <77040582+The-Spaghetti-Is-Ready@users.noreply.github.com>
2024-03-12 12:12:34 +0800

    Added Scene class for ECS registry and other api 'scenes'
    the scene class has now been implemented into the cmake projects. This scene class will serve as the 'game' application running over the engine. Essentially a scene would be defined in whichever entrypoint/main app we decide to use for games.

---
+++ Bottlebrush_Engine/Bottlebrush_Engine/BBScene/Scene.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Marco Garzon Lara on 12/3/2024.
+//
+
+#include "Scene.h"
---
+++ Bottlebrush_Engine/Bottlebrush_Engine/BBScene/Scene.h
@@ -0,0 +1,19 @@
+//
+// Created by Marco Garzon Lara on 12/3/2024.
+//
+
+#include <entt/entt.hpp>
+
+#ifndef BOTTLEBRUSH_ENGINE_SCENE_H
+#define BOTTLEBRUSH_ENGINE_SCENE_H
+
+class Scene
+{
+public:
+
+private:
+    entt::registry registry;
+    ///TODO: This Scene will need a camera, Assimp scene, Physics api scene, and our Lua system in the near future.
+};
+
+#endif //BOTTLEBRUSH_ENGINE_SCENE_H
