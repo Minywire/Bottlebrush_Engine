59857ec586771efa4131f539e31e3d4284f91df0
Marco Garzon <77040582+The-Spaghetti-Is-Ready@users.noreply.github.com>
2024-03-12 12:16:20 +0800

    Updated Scene header to pragma and Added a comment on the ecs registry definition
    The comment explains what it is. I changed the header for the sake of consistency across headers.

--- Bottlebrush_Engine/Bottlebrush_Engine/BBScene/Scene.h
+++ Bottlebrush_Engine/Bottlebrush_Engine/BBScene/Scene.h
@@ -1,19 +1,15 @@
 //
 // Created by Marco Garzon Lara on 12/3/2024.
 //
+#pragma once
 
 #include <entt/entt.hpp>
-
-#ifndef BOTTLEBRUSH_ENGINE_SCENE_H
-#define BOTTLEBRUSH_ENGINE_SCENE_H
 
 class Scene
 {
 public:
 
 private:
-    entt::registry registry;
+    entt::registry registry; ///The registry for ECS management (basically a big array under the hood)
     ///TODO: This Scene will need a camera, Assimp scene, Physics api scene, and our Lua system in the near future.
 };
-
-#endif //BOTTLEBRUSH_ENGINE_SCENE_H
