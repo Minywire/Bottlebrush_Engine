82575d2934420c02d79391f6fa00ea268c20ee23
Niamh Wilson <niamhlwilson@gmail.com>
2024-03-14 09:32:01 +0800

    Create initial BBAssetImporter class
    I'm currently waiting on ecs implementation so that I can link the assimp scene into our scene class.

--- Bottlebrush_Engine/Bottlebrush_Engine/BBAssetImporter/BBAssetImporter.cpp
+++ Bottlebrush_Engine/Bottlebrush_Engine/BBAssetImporter/BBAssetImporter.cpp
@@ -2,3 +2,12 @@
 // Created by Marco on 09/03/24.
 //
 #include "BBAssetImporter.h"
+BBAssetImporter::BBAssetImporter() {
+// import.SetPropertyInteger?
+}
+bool BBAssetImporter::loadModel(const std::string &filePath) {
+  // TODO: implement assimp scene in the importer when ecs is merged
+  // NOTE This might be able to be made static when all is said and done.
+  // If the scene exists, then return true, otherwise return false
+  return false;
+}
--- Bottlebrush_Engine/Bottlebrush_Engine/BBAssetImporter/BBAssetImporter.h
+++ Bottlebrush_Engine/Bottlebrush_Engine/BBAssetImporter/BBAssetImporter.h
@@ -8,6 +8,17 @@
 #include <assimp/postprocess.h>
 
 
+class BBAssetImporter {
+ public:
+  BBAssetImporter();
+
+  bool loadModel(const std::string& filePath);
+
+ private:
+  Assimp::Importer import;
+};
+
+
 
 
 
