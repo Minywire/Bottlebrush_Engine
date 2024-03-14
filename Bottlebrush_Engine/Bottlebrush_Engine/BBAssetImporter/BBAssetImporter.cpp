4aeda3880b9f20f4eabbef5cb424b4ff7f7fd194
Niamh Wilson <niamhlwilson@gmail.com>
2024-03-14 10:00:30 +0800

    Loads opens the filepath, loads the model, and returns true or false if the model is loaded.
    I need to set up a google test for the function

--- Bottlebrush_Engine/Bottlebrush_Engine/BBAssetImporter/BBAssetImporter.cpp
+++ Bottlebrush_Engine/Bottlebrush_Engine/BBAssetImporter/BBAssetImporter.cpp
@@ -6,8 +6,12 @@ BBAssetImporter::BBAssetImporter() {
 // import.SetPropertyInteger?
 }
 bool BBAssetImporter::loadModel(const std::string &filePath) {
-  // TODO: implement assimp scene in the importer when ecs is merged
+  const aiScene* scene = import.ReadFile(filePath, aiProcess_Triangulate);
+
+  if(!scene) {
+    return false;
+  }
   // NOTE This might be able to be made static when all is said and done.
   // If the scene exists, then return true, otherwise return false
-  return false;
+  return true;
 }
--- Bottlebrush_Engine/Bottlebrush_Engine/BBAssetImporter/BBAssetImporter.h
+++ Bottlebrush_Engine/Bottlebrush_Engine/BBAssetImporter/BBAssetImporter.h
@@ -7,11 +7,21 @@
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
 
-
+/**
+ * @class BBAssetImporter
+ * By default, this will use the right handed coordinate system, the same as opengl.
+ * The data is stored in a tree structure that lets you access all nodes, meshes, materials, animations or textures that were read from the imported file
+ */
 class BBAssetImporter {
  public:
   BBAssetImporter();
 
+  /**
+   * @brief Loads a 3D model from a given file path
+   * @param filePath The filepath of the model
+   * @return true or false if the model loaded.
+   * @author Niamh Wilson
+   */
   bool loadModel(const std::string& filePath);
 
  private:
