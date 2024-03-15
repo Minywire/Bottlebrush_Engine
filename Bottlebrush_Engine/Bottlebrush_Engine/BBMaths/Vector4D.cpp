6e2370fb2495260519e8126abc5041cece54a3cb
Jaiden di Lanzo <jaiden.dilanzo@bigpond.com>
2024-03-15 08:14:53 +0800

    Add 4D vector classes

---
+++ Bottlebrush_Engine/Bottlebrush_Engine/BBMaths/Vector4D.cpp
@@ -0,0 +1,160 @@
+#include "Vector4D.h"
+
+namespace BBMaths {
+Vector4D& Vector4D::Set(float a, float b, float c, float d) {
+  x_ = a;
+  y_ = b;
+  z_ = c;
+  w_ = d;
+  return (*this);
+}
+
+Vector4D& Vector4D::Set(const Vector3D& v) {
+  x_ = v.x_;
+  y_ = v.y_;
+  z_ = v.z_;
+  w_ = 0.0f;
+  return (*this);
+}
+
+Vector4D& Vector4D::Set(const Vector3D& v, float d) {
+  x_ = v.x_;
+  y_ = v.y_;
+  z_ = v.z_;
+  w_ = d;
+  return (*this);
+}
+
+Vector4D& Vector4D::Set(const Vector2D& v) {
+  x_ = v.x_;
+  y_ = v.y_;
+  z_ = 0.0f;
+  w_ = 0.0f;
+  return (*this);
+}
+
+Vector4D& Vector4D::Set(const Vector2D& v, float c, float d) {
+  x_ = v.x_;
+  y_ = v.y_;
+  z_ = c;
+  w_ = d;
+  return (*this);
+}
+
+Vector4D& Vector4D::Normalise() {
+  float n = InverseMagnitude(*this);
+  x_ = x_ * n;
+  y_ = y_ * n;
+  z_ = z_ * n;
+  w_ = w_ * n;
+  return (*this);
+}
+
+Vector4D& Vector4D::RotateAboutX(float angle) {
+  float y = y_, z = z_;
+  y_ = y * Cos(angle) - z * Sin(angle);
+  z_ = y * Sin(angle) + z * Cos(angle);
+  return (*this);
+}
+
+Vector4D& Vector4D::RotateAboutY(float angle) {
+  float x = x_, z = z_;
+  x_ = x * Cos(angle) + z * Sin(angle);
+  z_ = z * Cos(angle) - x * Sin(angle);
+  return (*this);
+}
+
+Vector4D& Vector4D::RotateAboutZ(float angle) {
+  float x = x_, y = y_;
+  x_ = x * Cos(angle) - y * Sin(angle);
+  y_ = x * Sin(angle) + y * Cos(angle);
+  return (*this);
+}
+
+Vector4D& Vector4D::operator=(const Vector4D& v) {
+  x_ = v.x_;
+  y_ = v.y_;
+  z_ = v.z_;
+  w_ = v.w_;
+  return (*this);
+}
+
+Vector4D& Vector4D::operator+=(const Vector4D& v) {
+  x_ += v.x_;
+  y_ += v.y_;
+  z_ += v.z_;
+  w_ += v.w_;
+  return (*this);
+}
+
+Vector4D& Vector4D::operator-=(const Vector4D& v) {
+  x_ -= v.x_;
+  y_ -= v.y_;
+  z_ -= v.z_;
+  w_ -= v.w_;
+  return (*this);
+}
+
+Vector4D& Vector4D::operator*=(float n) {
+  x_ *= n;
+  y_ *= n;
+  z_ *= n;
+  w_ *= n;
+  return (*this);
+}
+
+Vector4D& Vector4D::operator/=(float n) {
+  x_ /= n;
+  y_ /= n;
+  z_ /= n;
+  w_ /= n;
+  return (*this);
+}
+
+float Dot(const Vector4D& a, const Vector4D& b) {
+  return (a.x_ * b.x_ + a.y_ * b.y_ + a.z_ * b.z_ + a.w_ * b.w_);
+}
+
+float Magnitude(const Vector4D& v) {
+  return (Sqrt(v.x_ * v.x_ + v.y_ * v.y_ + v.z_ * v.z_ + v.w_ * v.w_));
+}
+
+float InverseMagnitude(const Vector4D& v) {
+  return (InverseSqrt(v.x_ * v.x_ + v.y_ * v.y_ + v.z_ * v.z_ + v.w_ * v.w_));
+}
+
+float SquaredMagnitude(const Vector4D& v) {
+  return (v.x_ * v.x_ + v.y_ * v.y_ + v.z_ * v.z_ + v.w_ * v.w_);
+}
+
+Vector4D Projection(const Vector4D& a, const Vector4D& b) {
+  return (b * Dot(a, b) * 1.0f / Dot(b, b));
+}
+
+Vector4D Rejection(const Vector4D& a, const Vector4D& b) {
+  return (a - b * Dot(a, b) * 1.0f / Dot(b, b));
+}
+
+Vector4D operator-(const Vector4D& v) { return {-v.x_, -v.y_, -v.z_, -v.w_}; }
+
+Vector4D operator+(const Vector4D& a, const Vector4D& b) {
+  return {a.x_ + b.x_, a.y_ + b.y_, a.z_ + b.z_, a.w_ + b.w_};
+}
+
+Vector4D operator-(const Vector4D& a, const Vector4D& b) {
+  return {a.x_ - b.x_, a.y_ - b.y_, a.z_ - b.z_, a.w_ - b.w_};
+}
+
+Vector4D operator*(const Vector4D& v, float n) {
+  return {v.x_ * n, v.y_ * n, v.z_ * n, v.w_ * n};
+}
+
+Vector4D operator*(float n, const Vector4D& v) {
+  return {n * v.x_, n * v.y_, n * v.z_, n * v.w_};
+}
+
+Vector4D operator/(const Vector4D& v, float n) {
+  n = 1.0f / n;
+  return {v.x_ * n, v.y_ * n, v.z_ * n, v.w_ * n};
+}
+}  // namespace BBMaths
---
+++ Bottlebrush_Engine/Bottlebrush_Engine/BBMaths/Vector4D.h
@@ -0,0 +1,146 @@
+#ifndef BOTTLEBRUSH_ENGINE_VECTOR4D_H_
+#define BOTTLEBRUSH_ENGINE_VECTOR4D_H_
+
+#include "Maths.h"
+#include "Vector2D.h"
+#include "Vector3D.h"
+
+namespace BBMaths {
+typedef float TypeVec4D;
+
+/// @class Vec4D
+/// @brief Vectors use a generic base class to store their components.
+template <typename T>
+class Vec4D {
+ public:
+  T x_;
+  T y_;
+  T z_;
+  T w_;
+
+ protected:
+  inline Vec4D() = default;
+
+  Vec4D(const T& a, const T& b, const T& c, const T& d) {
+    x_ = a;
+    y_ = b;
+    z_ = c;
+    w_ = d;
+  }
+
+  explicit Vec4D(const Vec4D<TypeVec4D>& v) {
+    x_ = v.x_;
+    y_ = v.y_;
+    z_ = v.z_;
+    w_ = v.w_;
+  }
+
+  explicit Vec4D(const Vec3D<TypeVec3D>& v) {
+    x_ = v.x_;
+    y_ = v.y_;
+    z_ = v.z_;
+    w_ = 0.0f;
+  }
+
+  Vec4D(const Vec3D<TypeVec3D>& v, float d) {
+    x_ = v.x_;
+    y_ = v.y_;
+    z_ = v.z_;
+    w_ = d;
+  }
+
+  explicit Vec4D(const Vec2D<TypeVec2D>& v) {
+    x_ = v.x_;
+    y_ = v.y_;
+    z_ = 0.0f;
+    w_ = 0.0f;
+  }
+
+  Vec4D(const Vec2D<TypeVec2D>& v, float c, float d) {
+    x_ = v.x_;
+    y_ = v.y_;
+    z_ = c;
+    w_ = d;
+  }
+};
+
+/// @class Vector4D
+/// @brief The \c Vector4D class encapsulates a 3D vector.
+///
+/// The \c Vector4D class is used to represent four-dimensional directional
+/// vectors in Euclidean geometric space with real-valued components <i>x</i>,
+/// <i>y</i>, <i>z</i>, and <i>w</i>. A directional vector of this class is
+/// assumed to have a <i>w</i> co-ordinate of 0 when constructed from a
+/// two-dimensional or three-dimensional vector, or a <i>w</i> co-ordinate of 1
+/// when constructed from a two-dimensional or three-dimensional point.
+class Vector4D : public Vec4D<TypeVec4D> {
+ public:
+  inline Vector4D() = default;
+
+  Vector4D(float a, float b, float c, float d) : Vec4D<TypeVec4D>(a, b, c, d) {}
+
+  explicit Vector4D(const Vec4D<TypeVec4D>& v) : Vec4D<TypeVec4D>(v) {}
+
+  explicit Vector4D(const Vector3D& v) : Vec4D<TypeVec4D>(v) {}
+
+  Vector4D(const Vector3D& v, float d) : Vec4D<TypeVec4D>(v, d) {}
+
+  explicit Vector4D(const Vector2D& v) : Vec4D<TypeVec4D>(v) {}
+
+  Vector4D(const Vector2D& v, float c, float d) : Vec4D<TypeVec4D>(v, c, d) {}
+
+  Vector4D& Set(float a, float b, float c, float d);
+
+  Vector4D& Set(const Vector3D& v);
+
+  Vector4D& Set(const Vector3D& v, float d);
+
+  Vector4D& Set(const Vector2D& v);
+
+  Vector4D& Set(const Vector2D& v, float c, float d);
+
+  Vector4D& Normalise();
+
+  Vector4D& RotateAboutX(float angle);
+
+  Vector4D& RotateAboutY(float angle);
+
+  Vector4D& RotateAboutZ(float angle);
+
+  Vector4D& operator=(const Vector4D& v);
+
+  Vector4D& operator+=(const Vector4D& v);
+
+  Vector4D& operator-=(const Vector4D& v);
+
+  Vector4D& operator*=(float n);
+
+  Vector4D& operator/=(float n);
+};
+
+float Dot(const Vector4D& a, const Vector4D& b);
+
+float Magnitude(const Vector4D& v);
+
+float InverseMagnitude(const Vector4D& v);
+
+float SquaredMagnitude(const Vector4D& v);
+
+Vector4D Projection(const Vector4D& a, const Vector4D& b);
+
+Vector4D Rejection(const Vector4D& a, const Vector4D& b);
+
+Vector4D operator-(const Vector4D& v);
+
+Vector4D operator+(const Vector4D& a, const Vector4D& b);
+
+Vector4D operator-(const Vector4D& a, const Vector4D& b);
+
+Vector4D operator*(const Vector4D& v, float n);
+
+Vector4D operator*(float n, const Vector4D& v);
+
+Vector4D operator/(const Vector4D& v, float n);
+}  // namespace BBMaths
+
+#endif  // BOTTLEBRUSH_ENGINE_VECTOR4D_H_
