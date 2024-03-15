3d0d96f357c9b53498e4ddbec4b7da34846c2f2e
Jaiden di Lanzo <jaiden.dilanzo@bigpond.com>
2024-03-15 08:17:41 +0800

    Change InverseSqrt implementation to use std::sqrt

--- Bottlebrush_Engine/Bottlebrush_Engine/BBMaths/Maths.cpp
+++ Bottlebrush_Engine/Bottlebrush_Engine/BBMaths/Maths.cpp
@@ -9,9 +9,5 @@ float Tan(float angle) { return std::tan(angle); }
 
 float Sqrt(float x) { return (std::sqrt(x)); }
 
-float InverseSqrt(float x) {
-  auto const y =
-      std::bit_cast<float>(0x5f3759df - (std::bit_cast<std::uint32_t>(x) >> 1));
-  return y * (1.5f - (x * 0.5f * y * y));
-}
+float InverseSqrt(float x) { return (1.0f / std::sqrt(x)); }
 }  // namespace BBMaths
