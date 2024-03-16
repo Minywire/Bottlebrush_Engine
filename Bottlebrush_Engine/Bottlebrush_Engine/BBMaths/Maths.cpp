9ec270b7f23d0d2c14f7183a9f90029119d51b80
Jaiden di Lanzo <jaiden.dilanzo@bigpond.com>
2024-03-16 17:52:20 +0800

    Minor syntactic sugar changes

--- Bottlebrush_Engine/Bottlebrush_Engine/BBMaths/Maths.cpp
+++ Bottlebrush_Engine/Bottlebrush_Engine/BBMaths/Maths.cpp
@@ -8,8 +8,8 @@ float Sin(float x) { return std::sin(x); }
 
 float Tan(float x) { return std::tan(x); }
 
-float Sqrt(float x) { return (std::sqrt(x)); }
+float Sqrt(float x) { return std::sqrt(x); }
 
-float InverseSqrt(float x) { return (1.0f / std::sqrt(x)); }
+float InverseSqrt(float x) { return 1.0f / std::sqrt(x); }
 
 }  // namespace BBMaths
