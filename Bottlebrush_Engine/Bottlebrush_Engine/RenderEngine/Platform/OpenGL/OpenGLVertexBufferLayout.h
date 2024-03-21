f92993e36edb531fbbc8d51ff7797c5d0697edab
Marco Garzon <77040582+The-Spaghetti-Is-Ready@users.noreply.github.com>
2024-03-21 17:05:58 +0800

    Set Up Initial CMake Project Folders for Scripting Component
    Changelog:
    - Added Lua and Sol scripting includes/dependencies
    - Added BBScript engine component folder

--- Bottlebrush_Engine/Bottlebrush_Engine/RenderEngine/Platform/OpenGL/OpenGLVertexBufferLayout.h
+++ Bottlebrush_Engine/Bottlebrush_Engine/RenderEngine/Platform/OpenGL/OpenGLVertexBufferLayout.h
@@ -19,7 +19,7 @@ static unsigned int GetSizeOfType(unsigned int type)
 	case GL_UNSIGNED_INT:		return 4;
 	case GL_UNSIGNED_BYTE:		return 1;
 	}
-	ASSERT(false);
+	//ASSERT(false);
 	return 0;
 }
 
@@ -33,7 +33,7 @@ public:
 	void Push(unsigned int count)
 	{
 		// if T type is unmatched
-		std::runtime_error(false);
+		std::runtime_error("false");
 	}
 
 	template<>
