54cd477b78b0c54536fdcbbbeec3f04593969423
Jaiden di Lanzo <jaiden.dilanzo@bigpond.com>
2024-04-02 19:42:38 +0800

    Remove DegToRad and RadToDeg functions
    This functionality is provided in GLM using glm::degrees and glm::radians

--- Bottlebrush_Engine/Bottlebrush_Engine/BBMaths/Maths.h
+++ Bottlebrush_Engine/Bottlebrush_Engine/BBMaths/Maths.h
@@ -1,27 +1,6 @@
 #ifndef BOTTLEBRUSH_ENGINE_MATHS_H
 #define BOTTLEBRUSH_ENGINE_MATHS_H
 
-#include <cmath>
-#include <limits>
-#include <numbers>
-#include <numeric>
-
 #include "glm/glm.hpp"
-
-namespace BBMaths {
-
-/// @brief Evaluates the conversion from degrees to radians.
-/// @param degrees The real-valued number in degrees.
-/// @returns The value of \c degrees evaluated such that \a R = \a D ⋅ \a π /
-/// 180.
-float DegToRad(float degrees);
-
-/// @brief Evaluates the conversion from radians to degrees.
-/// @param degrees The real-valued number in radians.
-/// @returns The value of \c radians evaluated such that \a D = \a R ⋅ 180 / \a
-/// π.
-float RadToDeg(float radians);
-
-}  // namespace BBMaths
 
 #endif  // BOTTLEBRUSH_ENGINE_MATHS_H
