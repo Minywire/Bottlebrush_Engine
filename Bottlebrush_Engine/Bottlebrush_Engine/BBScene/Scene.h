d0cf3cf72e0cc1ff8db9bad691f668c84291dabc
Marco Garzon <77040582+The-Spaghetti-Is-Ready@users.noreply.github.com>
2024-04-07 19:43:08 +0800

    Added Lua Master File field in Scene
    - Scene class now contains a string field containing the lua file name.

--- Bottlebrush_Engine/Bottlebrush_Engine/BBScene/Scene.h
+++ Bottlebrush_Engine/Bottlebrush_Engine/BBScene/Scene.h
@@ -4,6 +4,7 @@
 #pragma once
 
 #include "ECS.h"
+#include <string>
 #include <sol/sol.hpp>
 
 /**
@@ -14,13 +15,15 @@
 class Scene
 {
 public:
-    Scene(std::string lua_master);
+    Scene(std::string & lua_master) : master_lua_file(lua_master) {};
 
     /**
      *@brief Default constructor.
      */
     Scene();
 private:
+    std::string master_lua_file;
+
     ECS bb_ecs; ///The ECS object containing the enTT registry.
 
 };
