b3ff5376f620d589f5d201159f7fe727218821ff
Jaiden di Lanzo <jaiden.dilanzo@bigpond.com>
2024-04-11 17:40:56 +0800

    Add 3D Camera class
    NB: Camera uses Euler angles, can change to quaternions later on

---
+++ Bottlebrush_Engine/Bottlebrush_Engine/BBGraphics/Camera.h
@@ -0,0 +1,48 @@
+#ifndef BOTTLEBRUSH_ENGINE_CAMERA_H
+#define BOTTLEBRUSH_ENGINE_CAMERA_H
+
+#include <string>
+
+#include "glad/glad.h"
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+enum CameraMovement { FORWARD, BACKWARD, LEFT, RIGHT, UP, DOWN };
+
+const float kYaw = -90.0f;
+const float kPitch = 0.0f;
+const float kSpeed = 2.5f;
+const float kSensitivity = 0.1f;
+const float kZoom = 45.0f;
+
+class Camera {
+ public:
+  glm::vec3 position_;
+  glm::vec3 front_;
+  glm::vec3 up_;
+  glm::vec3 right_;
+  glm::vec3 world_up_;
+  float yaw_;
+  float pitch_;
+  float movement_speed_;
+  float mouse_sensitivity_;
+  float zoom_;
+
+  explicit Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f),
+                  glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = kYaw,
+                  float pitch = kPitch);
+
+  Camera(float pos_x, float pos_y, float pos_z, float up_x, float up_y,
+         float up_z, float yaw = kYaw, float pitch = kPitch);
+
+  [[nodiscard]] glm::mat4 GetViewMatrix() const;
+  void ProcessKeyboard(CameraMovement direction, float delta);
+  void ProcessMouseMovement(float x_offset, float y_offset,
+                            GLboolean constrain_pitch = true);
+  void ProcessMouseScroll(float y_offset);
+
+ protected:
+  void Update();
+};
+
+#endif  // BOTTLEBRUSH_ENGINE_CAMERA_H
