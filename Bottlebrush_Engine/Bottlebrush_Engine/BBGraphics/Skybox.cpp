2e1d8b1247d5a1a31898768e2e5c42755e6c3730
Idraliski <ultra4423@hotmail.com>
2024-04-17 10:39:01 +0800

    Skybox Obj

--- Bottlebrush_Engine/Bottlebrush_Engine/BBGraphics/Skybox.cpp
+++ Bottlebrush_Engine/Bottlebrush_Engine/BBGraphics/Skybox.cpp
@@ -4,6 +4,10 @@ Skybox::Skybox(std::filesystem::path model) {
   InitMesh(model);
 }
 
+Skybox::~Skybox() {
+
+}
+
 void Skybox::InitMesh(std::filesystem::path model) {
   m_Model = GraphicsFactory<GraphicsAPI::OpenGL>::CreateModel(
       model.string());
