90c529f00c4d21d5311077a40f8c9221a17653aa
Marco Garzon <77040582+The-Spaghetti-Is-Ready@users.noreply.github.com>
2024-04-18 02:08:28 +0800

    Reapply "Added Initial ResourceManagement Class and CMake"
    This reverts commit 249cd61f64a1e7aa66685803a5fa9f68e4a48f22.

---
+++ Bottlebrush_Engine/Bottlebrush_Engine/BBResourceManager/BBResourceManager.hpp
@@ -0,0 +1,15 @@
+//
+// Created by Marco on 17/04/24.
+//
+#pragma once
+
+#include <GraphicsFactory.h>
+#include <unordered_map>
+
+class BBResourceManager {
+public:
+
+private:
+    std::unordered_map<std::string, Model> sceneModels;
+
+};
