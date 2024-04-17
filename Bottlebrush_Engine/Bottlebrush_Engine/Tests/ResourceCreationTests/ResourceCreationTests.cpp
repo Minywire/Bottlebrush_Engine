85e0dfffe45f3217b7faa16e651f2381c1393a56
Marco Garzon <77040582+The-Spaghetti-Is-Ready@users.noreply.github.com>
2024-04-17 22:52:37 +0800

    Added Unit Testing Suite for ResourceManager

---
+++ Bottlebrush_Engine/Bottlebrush_Engine/Tests/ResourceCreationTests/ResourceCreationTests.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Marco on 17/04/24.
+//
+#include <gtest/gtest.h>
+
+#include <Scene.h>
+#include <BBResourceManager.hpp>
+#include <ECS.h>
+
+TEST(ResourceCreationTests, GenerateFromComponentTest)
+{
+
+}
