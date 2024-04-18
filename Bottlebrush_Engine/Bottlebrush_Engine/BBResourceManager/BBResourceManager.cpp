954966ae19315ba1c920101dbdb9609b760e4106
Marco Garzon <77040582+The-Spaghetti-Is-Ready@users.noreply.github.com>
2024-04-18 13:42:17 +0800

    Temporarily Removed Emplace Function to Test underlying Model Creation from Graphics Factory

--- Bottlebrush_Engine/Bottlebrush_Engine/BBResourceManager/BBResourceManager.cpp
+++ Bottlebrush_Engine/Bottlebrush_Engine/BBResourceManager/BBResourceManager.cpp
@@ -6,7 +6,8 @@
 
 void BBResourceManager::generateModelFromComponent(const ModelComponent & modelComp)
 {
-    sceneModels.emplace(std::pair<std::string, std::unique_ptr<Model>>(modelComp.model_path, GraphicsFactory<GraphicsAPI::OpenGL>::CreateModel(modelComp.model_path)));
+    GraphicsFactory<GraphicsAPI::OpenGL>::CreateModel(modelComp.model_path);
+    //sceneModels.emplace(std::pair<std::string, std::unique_ptr<Model>>(modelComp.model_path, GraphicsFactory<GraphicsAPI::OpenGL>::CreateModel(modelComp.model_path)));
 }
 
 const std::unordered_map<std::string, std::unique_ptr<Model>> & BBResourceManager::getSceneModels() const
