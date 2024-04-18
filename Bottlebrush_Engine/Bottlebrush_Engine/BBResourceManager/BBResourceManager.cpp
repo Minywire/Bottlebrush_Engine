0b6c42f1d1527737498fde76e956cf168540a586
Marco Garzon <77040582+The-Spaghetti-Is-Ready@users.noreply.github.com>
2024-04-18 15:53:30 +0800

    Added GLAD Init to Unit Testing and Now Using Map Emplace in Resource Manager
    Unit test for generating models from components has passed. The model is created and placed inside the map

--- Bottlebrush_Engine/Bottlebrush_Engine/BBResourceManager/BBResourceManager.cpp
+++ Bottlebrush_Engine/Bottlebrush_Engine/BBResourceManager/BBResourceManager.cpp
@@ -6,8 +6,7 @@
 
 void BBResourceManager::generateModelFromComponent(const ModelComponent & modelComp)
 {
-    GraphicsFactory<GraphicsAPI::OpenGL>::CreateModel(modelComp.model_path);
-    //sceneModels.emplace(std::pair<std::string, std::unique_ptr<Model>>(modelComp.model_path, GraphicsFactory<GraphicsAPI::OpenGL>::CreateModel(modelComp.model_path)));
+    sceneModels.emplace(std::pair<std::string, std::unique_ptr<Model>>(modelComp.model_path, GraphicsFactory<GraphicsAPI::OpenGL>::CreateModel(modelComp.model_path)));
 }
 
 const std::unordered_map<std::string, std::unique_ptr<Model>> & BBResourceManager::getSceneModels() const
