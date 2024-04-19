904cb7b47cf47924015b7f165a6c8d30fb1985b6
Marco Garzon <77040582+The-Spaghetti-Is-Ready@users.noreply.github.com>
2024-04-19 17:13:57 +0800

    Added Doxygen For ResourceManager Class

--- Bottlebrush_Engine/Bottlebrush_Engine/BBResourceManager/BBResourceManager.hpp
+++ Bottlebrush_Engine/Bottlebrush_Engine/BBResourceManager/BBResourceManager.hpp
@@ -6,24 +6,27 @@
 #include <ECS.h>
 #include <GraphicsFactory.h>
 #include <unordered_map>
-
+/**
+ * @class BBResourceManager
+ * @brief Stores data generated from ECS systems which utilise metadata that is saved into the ECS Entities.
+ */
 class BBResourceManager {
 public:
     /**
      *
-     * @param modelComp
+     * @param modelComp The Model component queried from an entity
      */
     void generateModelFromComponent(const ModelComponent & modelComp);
 
     /**
      *
-     * @return
+     * @return A reference map structure contain all the model data stored for the scene (read usage)
      */
     const std::unordered_map<std::string, std::unique_ptr<Model>> & getSceneModels() const;
 
     /**
      *
-     * @return
+     * @return A reference map structure contain all the model data stored for the scene (write usage)
      */
     std::unordered_map<std::string, std::unique_ptr<Model>> & getSceneModels();
 
