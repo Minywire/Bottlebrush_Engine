517420bb66c14a3d12c47f513de54200c180210d
Marco Garzon <77040582+The-Spaghetti-Is-Ready@users.noreply.github.com>
2024-04-20 13:41:45 +0800

    Added scripted tree and house instances
    house is currently commented out in the master script

--- Bottlebrush_Engine/Bottlebrush_Engine/Game/master_file.lua
+++ Bottlebrush_Engine/Bottlebrush_Engine/Game/master_file.lua
@@ -2,10 +2,16 @@
 
 create_entity("Game/GrassCube.lua");
 
-for start=0, 5000, 1000 do
-    for start1=0, 5000, 1000 do
-        create_entityTR("Game/PineTree.lua", 3750 + start, 0, 2500 + start1); --Create a line of 10 instances
+for start=0, 4000, 1000 do
+    for start1=0, 4000, 1000 do
+        create_entityTR("Game/PineTree.lua", 4750 + start, 0, 2500 + start1); --Create a grid of 16 instances
     end
 end
+
+--for start=0, 5000, 1000 do
+--    for start1=0, 3000, 1000 do
+--        create_entityTR("Game/House.lua", 6000 + start, 0, 4550 + start1); --Create a grid of 15 instances
+--    end
+--end
 
 
