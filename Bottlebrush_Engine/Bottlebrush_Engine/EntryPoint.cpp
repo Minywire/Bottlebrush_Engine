36210c2d27d3c4c8965b2ec6527f7f69996e2216
Marco Garzon <77040582+The-Spaghetti-Is-Ready@users.noreply.github.com>
2024-04-20 16:33:13 +0800

    Added Splashscreen with group and also fixed terrain math

--- Bottlebrush_Engine/Bottlebrush_Engine/EntryPoint.cpp
+++ Bottlebrush_Engine/Bottlebrush_Engine/EntryPoint.cpp
@@ -23,12 +23,14 @@ glm::vec3 terrain_scale = {3.5f, 0.25f, 3.5f},
 Camera camera(glm::vec3(0.0f, 25.0f, 0.0f));
 float last_x = screen_width / 2.0f, last_y = screen_height / 2.0f;
 bool first_mouse_click = true;
+
 bool exitScreen = false;
 
 // Timing
 float delta = 0.0f, last_frame = 0.0f;
 
 void ProcessInput(GLFWwindow *window) {
+
   if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
       exitScreen = true;
   }
@@ -53,9 +55,11 @@ void ProcessInput(GLFWwindow *window) {
     camera.ProcessKeyboard(DOWN, delta);
 }
 
+
 void FramebufferSizeCallback(GLFWwindow *window, int width, int height) {
   glViewport(0, 0, width, height);
 }
+
 
 void KeyCallback(GLFWwindow *window, int key, int scancode, int action,
                  int mods) {
@@ -70,6 +74,7 @@ void MouseCallback(GLFWwindow *window, double pos_x, double pos_y) {
     last_y = y;
     first_mouse_click = false;
   }
+
 
   float x_offset = x - last_x, y_offset = last_y - y;
 
@@ -216,10 +221,13 @@ int main() {
       terrain_height -= terrain_height_diff * delta;
     if (terrain_height > terrain_height_init)
       terrain_height += terrain_height_diff * delta;
-    if (terrain_height < 0)
-      camera.position_.y = -terrain_shift.y * terrain_scale.y;
-    else
-      camera.position_.y = terrain_height * terrain_scale.y;
+    if(!exitScreen)
+    {
+        if (terrain_height < 0)
+          camera.position_.y = -terrain_shift.y * terrain_scale.y;
+        else
+          camera.position_.y = terrain_height * terrain_scale.y;
+    }
 
      //MY SCENE
     gameScene.setProjectionMatrix(projection);
@@ -280,6 +288,16 @@ int main() {
     }
 
     glDepthFunc(GL_LESS);  // set depth function back to default
+
+    if(exitScreen)
+    {
+        camera.position_.x = -4825;
+        camera.position_.y = 0;
+        camera.position_.z = -5000;
+        camera.movement_speed_ = 0;
+        camera.front_ = {-5000, 0 , 0};
+        camera.up_ = {0,1,0};
+    }
 
     // Swap out buffers and poll for input events
     glfwSwapBuffers(window);
