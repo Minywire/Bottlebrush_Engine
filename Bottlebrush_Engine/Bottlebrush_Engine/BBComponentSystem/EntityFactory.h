9e59b83a79bd9c38f55777f400437e5e1cd4542f
Marco Garzon <77040582+The-Spaghetti-Is-Ready@users.noreply.github.com>
2024-04-20 17:18:11 +0800

    Added remaining doxy for my stuff

--- Bottlebrush_Engine/Bottlebrush_Engine/BBComponentSystem/EntityFactory.h
+++ Bottlebrush_Engine/Bottlebrush_Engine/BBComponentSystem/EntityFactory.h
@@ -15,65 +15,64 @@ public:
      * @brief Creates an entity from a lua file
      * @param ecs The component system object containing the specified registry to create the entity on
      * @param lua_file The specified Lua file to create the entity from
-     * @return
+     * @return created entity
      */
     Entity create_from_file(ECS &ecs, sol::state &lua_state, const std::filesystem::path &lua_file);
 
     /**
      *
-     * @param ecs
-     * @param lua_state
-     * @param lua_file
-     * @param xPos
-     * @param yPos
-     * @param zPos
-     * @return
+     * @param ecs The registry from which to create entities from
+     * @param lua_state the lua state
+     * @param lua_file the lua file to load from
+     * @param xPos x translate
+     * @param yPos y translate
+     * @param zPos z translate
+     * @return created entity
      */
     Entity create_from_file(ECS &ecs, sol::state &lua_state, const std::filesystem::path &lua_file, float xPos, float yPos, float zPos);
 
 private:
     /**
      *
-     * @param ecs
-     * @param entity
-     * @param table
+     * @param ecs The ecs to load components from
+     * @param entity entity to add components to
+     * @param table tables
      */
     void load_components(ECS &ecs, Entity &entity, const sol::table &table);
 
     /**
-     *
-     * @param ecs
-     * @param entity
-     * @param table
-     * @param xPos
-     * @param yPos
-     * @param zPos
+     * @param ecs  The ecs to load components from
+     * @param entity the entity to attach components to
+     * @param table the sol table to compare
+     * @param xPos x translate
+     * @param yPos y translate
+     * @param zPos z translate
      */
     void load_components(ECS &ecs, Entity &entity, const sol::table &table, float xPos, float yPos, float zPos);
 
     /**
      *
-     * @param entity
-     * @param transform
+     * @param entity the entity to load the transform component from
+     * @param transform the transform sol table to compare against
      */
     void loadTransform(ECS& ecs, Entity &entity, const sol::table &transform);
 
     /**
      *
-     * @param ecs
-     * @param entity
-     * @param transform
-     * @param xPos
-     * @param yPos
-     * @param zPos
+     * @param ecs the ecs to load the transform component from
+     * @param entity the entity to attach the component to
+     * @param transform the transform table to compare agains
+     * @param xPos x translate
+     * @param yPos y translate
+     * @param zPos z translate
      */
     void loadTransform(ECS& ecs, Entity &entity, const sol::table &transform, float xPos, float yPos, float zPos);
 
     /**
      *
-     * @param ecs
-     * @param entity
-     * @param model
+     * @param ecs the ecs registry to load model component from
+     * @param entity the entity to attach to
+     * @param model the model table to compare against
      */
     void loadModel(ECS& ecs, Entity &entity, const sol::table &model);
 };
