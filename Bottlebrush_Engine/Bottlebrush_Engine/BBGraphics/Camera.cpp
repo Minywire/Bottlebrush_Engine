28b648dceabecd3699017da879f6c0fae4a98e6c
Jaiden di Lanzo <jaiden.dilanzo@bigpond.com>
2024-04-25 11:38:06 +0800

    Add camera mutators and accessors + make member vars private

--- Bottlebrush_Engine/Bottlebrush_Engine/BBGraphics/Camera.cpp
+++ Bottlebrush_Engine/Bottlebrush_Engine/BBGraphics/Camera.cpp
@@ -1,38 +1,86 @@
 #include "Camera.h"
 
-Camera::Camera(glm::vec3 position, glm::vec3 up, float yaw, float pitch)
-    : front_(glm::vec3(0.0f, 0.0f, -1.0f)),
-      movement_speed_(kSpeed),
-      mouse_sensitivity_(kSensitivity),
-      zoom_(kMaxZoom) {
+Camera::Camera(glm::vec3 position) {
+  front_ = {0.0f, 0.0f, -1.0f};
+  world_up_ = {0.0f, 1.0f, 0.0f};
+  sensitivity_ = kSensitivity;
+  speed_ = kSpeed;
+  pitch_ = kPitch;
   position_ = position;
-  world_up_ = up;
-  yaw_ = yaw;
-  pitch_ = pitch;
+  yaw_ = kYaw;
+  zoom_ = kMaxZoom;
 
   Update();
 }
 
-Camera::Camera(float pos_x, float pos_y, float pos_z, float up_x, float up_y,
-               float up_z, float yaw, float pitch)
-    : front_(glm::vec3(0.0f, 0.0f, -1.0f)),
-      movement_speed_(kSpeed),
-      mouse_sensitivity_(kSensitivity),
-      zoom_(kMaxZoom) {
-  position_ = glm::vec3(pos_x, pos_y, pos_z);
-  world_up_ = glm::vec3(up_x, up_y, up_z);
-  yaw_ = yaw;
-  pitch_ = pitch;
+Camera::Camera(float x, float y, float z) {
+  front_ = {0.0f, 0.0f, -1.0f};
+  world_up_ = {0.0f, 1.0f, 0.0f};
+  sensitivity_ = kSensitivity;
+  speed_ = kSpeed;
+  pitch_ = kPitch;
+  position_ = {x, y, z};
+  yaw_ = kYaw;
 
   Update();
 }
 
-glm::mat4 Camera::GetViewMatrix() const {
-  return glm::lookAt(position_, position_ + front_, up_);
+glm::vec3 Camera::GetFront() const { return front_; }
+
+float Camera::GetSensitivity() const { return sensitivity_; }
+
+float Camera::GetSpeed() const { return speed_; }
+
+float Camera::GetPitch() const { return pitch_; }
+
+glm::vec3 Camera::GetPosition() const { return position_; }
+
+float Camera::GetPositionX() const { return position_.x; }
+
+float Camera::GetPositionY() const { return position_.y; }
+
+float Camera::GetPositionZ() const { return position_.z; }
+
+glm::vec3 Camera::GetRight() const { return right_; }
+
+glm::vec3 Camera::GetUp() const { return up_; }
+
+glm::mat4 Camera::GetViewMatrix() const { return look_at_; }
+
+glm::vec3 Camera::GetWorldUp() const { return world_up_; }
+
+float Camera::GetZoom() const { return zoom_; }
+
+float Camera::GetYaw() const { return yaw_; }
+
+void Camera::SetFront(float x, float y, float z) { front_ = {x, y, z}; }
+
+void Camera::SetSensitivity(float sensitivity) { sensitivity_ = sensitivity; }
+
+void Camera::SetSpeed(float speed) { speed_ = speed; }
+
+void Camera::SetPitch(float pitch) { pitch_ = pitch; }
+
+void Camera::SetPosition(glm::vec3 position) { position_ = position; }
+
+void Camera::SetPosition(float x, float y, float z) { position_ = {x, y, z}; }
+
+void Camera::SetPositionX(float x) { position_.x = x; }
+
+void Camera::SetPositionY(float y) { position_.y = y; }
+
+void Camera::SetPositionZ(float z) { position_.z = z; }
+
+void Camera::SetViewMatrix(glm::vec3 position, glm::vec3 front, glm::vec3 up) {
+  look_at_ = glm::lookAt(position, position + front, up);
 }
+
+void Camera::SetZoom(float zoom) { zoom_ = zoom; }
+
+void Camera::SetYaw(float yaw) { yaw_ = yaw; }
 
 void Camera::ProcessKeyboard(CameraMovement direction, float delta) {
-  float velocity = movement_speed_ * delta;
+  float velocity = speed_ * delta;
 
   if (direction == FORWARD) position_ += front_ * velocity;
   if (direction == BACKWARD) position_ -= front_ * velocity;
@@ -40,13 +88,14 @@ void Camera::ProcessKeyboard(CameraMovement direction, float delta) {
   if (direction == RIGHT) position_ += right_ * velocity;
   if (direction == UP) position_ += up_ * velocity;
   if (direction == DOWN) position_ -= up_ * velocity;
-  //  position_.y = 0.0f;
+
+  Update();
 }
 
 void Camera::ProcessMouseMovement(float x_offset, float y_offset,
                                   bool constrain_pitch) {
-  x_offset *= mouse_sensitivity_;
-  y_offset *= mouse_sensitivity_;
+  x_offset *= sensitivity_;
+  y_offset *= sensitivity_;
 
   yaw_ += x_offset;
   pitch_ += y_offset;
@@ -64,15 +113,16 @@ void Camera::ProcessMouseScroll(float y_offset) {
 
   if (zoom_ < kMinZoom) zoom_ = kMinZoom;
   if (zoom_ > kMaxZoom) zoom_ = kMaxZoom;
+
+  Update();
 }
 
 void Camera::Update() {
-  glm::vec3 front = glm::vec3(0.0f, 0.0f, -1.0f);
-
-  front.x = std::cos(glm::radians(yaw_)) * std::cos(glm::radians(pitch_));
-  front.y = std::sin(glm::radians(pitch_));
-  front.z = std::sin(glm::radians(yaw_)) * std::cos(glm::radians(pitch_));
-  front_ = glm::normalize(front);
+  front_.x = std::cos(glm::radians(yaw_)) * std::cos(glm::radians(pitch_));
+  front_.y = std::sin(glm::radians(pitch_));
+  front_.z = std::sin(glm::radians(yaw_)) * std::cos(glm::radians(pitch_));
+  front_ = glm::normalize(front_);
   right_ = glm::normalize(glm::cross(front_, world_up_));
   up_ = glm::normalize(glm::cross(right_, front_));
+  look_at_ = glm::lookAt(position_, position_ + front_, up_);
 }
--- Bottlebrush_Engine/Bottlebrush_Engine/BBGraphics/Camera.h
+++ Bottlebrush_Engine/Bottlebrush_Engine/BBGraphics/Camera.h
@@ -8,14 +8,14 @@
 
 enum CameraMovement { FORWARD, BACKWARD, LEFT, RIGHT, UP, DOWN };
 
-const float kYaw = -90.0f;
-const float kPitch = 0.0f;
-const float kSpeed = 2.5f;
-const float kSensitivity = 0.1f;
-const float kMaxZoom = 45.0f;
+const float kMaxZoom = 80.0f;
 const float kMinZoom = 1.0f;
 const float kMaxPitch = 89.0f;
 const float kMinPitch = -kMaxPitch;
+const float kPitch = 0.0f;
+const float kSensitivity = 0.1f;
+const float kSpeed = 2.5f;
+const float kYaw = -90.0f;
 
 /// @class Camera
 /// @brief The \c Camera class encapsulates a 3D camera.
@@ -30,23 +30,19 @@ const float kMinPitch = -kMaxPitch;
 /// @author Jaiden di Lanzo
 class Camera {
  public:
-  glm::vec3 position_;
-  glm::vec3 front_;
-  glm::vec3 up_;
-  glm::vec3 right_;
-  glm::vec3 world_up_;
-  float yaw_;
-  float pitch_;
-  float movement_speed_;
-  float mouse_sensitivity_;
-  float zoom_;
+  explicit Camera(glm::vec3 position = {0.0f, 0.0f, 0.0f});
+  Camera(float x, float y, float z);
 
-  explicit Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f),
-                  glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = kYaw,
-                  float pitch = kPitch);
-
-  Camera(float pos_x, float pos_y, float pos_z, float up_x, float up_y,
-         float up_z, float yaw = kYaw, float pitch = kPitch);
+  [[nodiscard]] glm::vec3 GetFront() const;
+  [[nodiscard]] float GetSensitivity() const;
+  [[nodiscard]] float GetSpeed() const;
+  [[nodiscard]] float GetPitch() const;
+  [[nodiscard]] glm::vec3 GetPosition() const;
+  [[nodiscard]] float GetPositionX() const;
+  [[nodiscard]] float GetPositionY() const;
+  [[nodiscard]] float GetPositionZ() const;
+  [[nodiscard]] glm::vec3 GetRight() const;
+  [[nodiscard]] glm::vec3 GetUp() const;
 
   /// @brief Gets the camera view matrix.
   /// <p>
@@ -55,6 +51,23 @@ class Camera {
   /// 4x4 matrix object.
   /// @returns A 4x4 matrix object that describes the conceptual look-at matrix.
   [[nodiscard]] glm::mat4 GetViewMatrix() const;
+
+  [[nodiscard]] glm::vec3 GetWorldUp() const;
+  [[nodiscard]] float GetZoom() const;
+  [[nodiscard]] float GetYaw() const;
+
+  void SetFront(float x, float y, float z);
+  void SetSensitivity(float sensitivity);
+  void SetSpeed(float speed);
+  void SetPitch(float pitch);
+  void SetPosition(glm::vec3 position);
+  void SetPosition(float x, float y, float z);
+  void SetPositionX(float x);
+  void SetPositionY(float y);
+  void SetPositionZ(float z);
+  void SetViewMatrix(glm::vec3 position, glm::vec3 front, glm::vec3 up);
+  void SetZoom(float zoom);
+  void SetYaw(float yaw);
 
   /// @brief Processes keyboard input movement applied to the camera.
   /// <p>
@@ -88,8 +101,20 @@ class Camera {
   /// @param y_offset The scroll movement offset in the \a y direction.
   void ProcessMouseScroll(float y_offset);
 
- protected:
+ private:
   void Update();
+
+  glm::vec3 front_;
+  glm::mat4 look_at_;
+  float pitch_;
+  glm::vec3 position_;
+  glm::vec3 right_;
+  float sensitivity_;
+  float speed_;
+  glm::vec3 up_;
+  glm::vec3 world_up_;
+  float yaw_;
+  float zoom_;
 };
 
 #endif  // BOTTLEBRUSH_ENGINE_CAMERA_H
