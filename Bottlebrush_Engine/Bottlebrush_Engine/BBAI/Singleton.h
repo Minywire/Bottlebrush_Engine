3d7cec089ad10c9aaf7eb355436cc0694bced1e6
Idraliski <ultra4423@hotmail.com>
2024-05-15 16:12:55 +0800

    More silly VS spacing

--- Bottlebrush_Engine/Bottlebrush_Engine/BBAI/Singleton.h
+++ Bottlebrush_Engine/Bottlebrush_Engine/BBAI/Singleton.h
@@ -12,28 +12,28 @@
 template <typename T>
 class Singleton
 {
-	public:
-            /// @author Alan
-            /// @brief returns an static instance of the object, checks if it exists first
-            /// @return a static object
-            static T& Instance()
-            {
-                // check if instance exists first
-                if (_instance == nullptr)
-                {
-                        _instance = new T();
-                }
-                else return *_instance;
-            }
+public:
+    /// @author Alan
+    /// @brief returns an static instance of the object, checks if it exists first
+    /// @return a static object
+    static T& Instance()
+    {
+        // check if instance exists first
+        if (_instance == nullptr)
+        {
+                _instance = new T();
+        }
+        else return *_instance;
+    }
 
-            // delete copy ctor
-            Singleton(Singleton const&) = delete;
-            Singleton& operator=(Singleton const&) = delete;
+    // delete copy ctor
+    Singleton(Singleton const&) = delete;
+    Singleton& operator=(Singleton const&) = delete;
 
-	private:
-            static std::unique_ptr<T> _instance;
+private:
+    static std::unique_ptr<T> _instance;
 
-            // hide ctor, dtor
-            Singleton() {};
-            ~Singleton() {};
+    // hide ctor, dtor
+    Singleton() {};
+    ~Singleton() {};
 };
