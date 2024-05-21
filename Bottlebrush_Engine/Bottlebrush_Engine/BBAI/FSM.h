81718576bc89f20ee5679497e7476205bbb7d5d0
Idraliski <ultra4423@hotmail.com>
2024-05-21 23:07:03 +0800

    Modified FSM to take in a lua_state instead of having its own

--- Bottlebrush_Engine/Bottlebrush_Engine/BBAI/FSM.h
+++ Bottlebrush_Engine/Bottlebrush_Engine/BBAI/FSM.h
@@ -7,9 +7,7 @@
 #include <string>
 #include <filesystem>
 
-#include "State.h"
 #include "BBScript.h"
-#include "Singleton.h"
 
 /// @author Alan
 /// @brief Finite State Machine for NPCs
@@ -30,22 +28,22 @@ public:
 	/// @author Alan
 	/// @brief normal update call for state machine
 	/// runs current state update, and global state update
-	void update()
+	void update(sol::state & lua_state)
 	{
-        if (m_globalState.compare("") != 0) m_lua.getLuaState()[m_globalState]["Update"](m_owner);
+        if (m_globalState.compare("") != 0) lua_state[m_globalState]["Update"](m_owner);
 
-        if (m_currentState.compare("") != 0) m_lua.getLuaState()[m_currentState]["Update"](m_owner);
+        if (m_currentState.compare("") != 0) lua_state[m_currentState]["Update"](m_owner);
 	}
 
 	/// @author Alan
 	/// @brief changes states, updates previous state, runs onExit and onEnter accordingly
 	/// @param newState the new state to transition to
-	void changeState(const std::string& newState) {
+	void changeState(const std::string& newState, sol::state & lua_state) {
         m_previousState = m_currentState;  // track previous state
-        m_lua.getLuaState()[m_currentState]["onExit"](m_owner);   // execute exit code
+        lua_state[m_currentState]["onExit"](m_owner);   // execute exit code
         m_currentState = newState;         // change states
-        m_lua.getLuaState()[m_currentState]["onEnter"](m_owner);  // execute enter code of new state
-    }  // @TODO find lua object to replace State<entity_type>
+        lua_state[m_currentState]["onEnter"](m_owner);  // execute enter code of new state
+    }
 
 	/// @author Alan
 	/// @brief reverts back to original state, used with globalState changing states temporarily
@@ -55,14 +53,14 @@ public:
 	/// @brief checks whether the param st is indeed what the current state is
 	/// @param st is the state to check whether the FSM is currently in
 	/// @return true or false of whether it is currently in that state or not
-	bool isInState(const std::string& st) const { return m_currentState.compare(st) == 0; } // @TODO find lua object to replace State<entity_type>
+	bool isInState(const std::string& st) const { return m_currentState.compare(st) == 0; }
 
 	// set states
-	void SetPreviousState(const std::string& st) { m_previousState = st; } // @TODO find lua object to replace State<entity_type>
+	void SetPreviousState(const std::string& st) { m_previousState = st; } 
 	void SetCurrentState(const std::string& st) { m_currentState = st; }
 	void SetGlobalState(const std::string& st) { m_globalState = st; }
 
-	std::string GetPreviousState() { return m_previousState; } // @TODO find lua object to replace State<entity_type>
+	std::string GetPreviousState() { return m_previousState; }
 	std::string GetCurrentState() { return m_currentState; }
 	std::string GetGlobalState() { return m_globalState; }
 
@@ -70,7 +68,6 @@ public:
 	{
 		if(path.extension() != ".lua") { throw std::runtime_error("Lua file is no lua file"); }
 		m_statePath = path;
-		m_lua.getLuaState().script_file(path.string());
 	}
 
 	std::filesystem::path GetStatePath() { return m_statePath; }
@@ -79,9 +76,8 @@ private:
 	entity_type* m_owner; // owning AI object
 
     std::filesystem::path m_statePath;
-    BBScript m_lua;
 
-	std::string m_previousState; // @TODO find lua object to replace State<entity_type>
+	std::string m_previousState;
 	std::string m_globalState;
 	std::string m_currentState;
 };
