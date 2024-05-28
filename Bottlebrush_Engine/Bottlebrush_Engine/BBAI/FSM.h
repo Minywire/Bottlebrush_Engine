363cc90ce3018d7ce849fc0b07316e49fa251409
Idraliski <ultra4423@hotmail.com>
2024-05-28 21:55:43 +0800

    update doxy

--- Bottlebrush_Engine/Bottlebrush_Engine/BBAI/FSM.h
+++ Bottlebrush_Engine/Bottlebrush_Engine/BBAI/FSM.h
@@ -28,6 +28,7 @@ public:
     /// @brief changes states, updates previous state, runs onExit and onEnter
     /// accordingly
     /// @param newState the new state to transition to
+    /// @param lua_state used to reference to the next section of the script to run
     void ChangeState(const std::string& newState, sol::state& lua_state);
 
     /// @author Alan
