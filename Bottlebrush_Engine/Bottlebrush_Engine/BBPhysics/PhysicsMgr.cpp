c51fb1b430735487a657ace316f3b44087149697
Jaiden di Lanzo <jaiden.dilanzo@bigpond.com>
2024-06-04 13:42:59 +0800

    Add CreatePhysicsBody method, add physics body map, make typedefs public

--- Bottlebrush_Engine/Bottlebrush_Engine/BBPhysics/PhysicsMgr.cpp
+++ Bottlebrush_Engine/Bottlebrush_Engine/BBPhysics/PhysicsMgr.cpp
@@ -7,6 +7,25 @@ PhysicsMgr& PhysicsMgr::GetInstance() {
     return instance_;
 }
 
+void PhysicsMgr::CreatePhysicsBody(PhysicsBody::PhysicsBodyType type,
+                                   glm::vec3 position, glm::vec3 rotation,
+                                   glm::vec3 scale) {
+    auto p = rp3d::Vector3(position.x, position.y, position.z);
+    auto o = rp3d::Quaternion::identity();
+    auto t = rp3d::Transform(p, o);
+    PhysicsBody::RigidBody rigid_body = world_->createRigidBody(t);
+
+    // Create a physics body and set its rigid body so that the physics body can
+    // manage its own data
+    PhysicsBody physics_body(type, position, rotation, scale);
+    physics_body.SetRigidBody(rigid_body);
+
+    // Put the physics body in the map using its physics world / entity index as
+    // a key so that the manager can reference it and manage it
+    uint32_t index = rigid_body->getEntity().getIndex();
+    physics_bodies_[index] = physics_body;
+}
+
 void PhysicsMgr::Update(float delta) { world_->update(delta); }
 
 PhysicsMgr::PhysicsMgr() { CreateWorld(); }
--- Bottlebrush_Engine/Bottlebrush_Engine/BBPhysics/PhysicsMgr.h
+++ Bottlebrush_Engine/Bottlebrush_Engine/BBPhysics/PhysicsMgr.h
@@ -1,17 +1,25 @@
 #ifndef BOTTLEBRUSH_ENGINE_PHYSICSMGR_H
 #define BOTTLEBRUSH_ENGINE_PHYSICSMGR_H
 
+#include <unordered_map>
+
+#include "PhysicsBody.h"
 #include "reactphysics3d/reactphysics3d.h"
 
 class PhysicsMgr {
+   public:
     using PhysicsCommon = rp3d::PhysicsCommon;
     using PhysicsWorld = rp3d::PhysicsWorld*;
 
-   public:
     ~PhysicsMgr();
 
     static PhysicsMgr& GetInstance();
 
+    void CreatePhysicsBody(PhysicsBody::PhysicsBodyType type =
+                               PhysicsBody::PhysicsBodyType::kStatic,
+                           glm::vec3 position = {0.0f, 0.0f, 0.0f},
+                           glm::vec3 rotation = {0.0f, 0.0f, 0.0f},
+                           glm::vec3 scale = {1.0f, 1.0f, 1.0f});
     void Update(float delta);
 
    private:
@@ -20,6 +28,7 @@ class PhysicsMgr {
     void CreateWorld();
     void DeleteWorld();
 
+    std::unordered_map<uint32_t, PhysicsBody> physics_bodies_;
     PhysicsCommon common_;
     PhysicsWorld world_;
 };
