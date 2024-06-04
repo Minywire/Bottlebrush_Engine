d596496bca12e6754aef179c68c53c540bc9ea78
Idraliski <ultra4423@hotmail.com>
2024-06-04 19:04:37 +0800

    Adjusted Scripts

---
+++ Bottlebrush_Engine/Bottlebrush_Engine/Game/AIStates/WanderStates.lua
@@ -0,0 +1,166 @@
+-------------------------------------------------------------------------------
+
+-- create some useful animation functions just in Lua
+
+-------------------------------------------------------------------------------
+
+function setMovingAnimation(NPC)
+	if NPC:IsMoving() then
+		Animation.SetAnimation(NPC, "run");
+	elseif not NPC:IsMoving() then
+		Animation.SetAnimation(NPC, "stand");
+	end
+end
+
+-------------------------------------------------------------------------------
+
+-- create the global state
+
+-------------------------------------------------------------------------------
+Global = {
+	onEnter = function(NPC)
+		
+	end,
+
+	Update = function(NPC)
+		setMovingAnimation(NPC);
+	end,
+
+	onExit = function(NPC)
+		
+	end,
+	
+	onMessage = function(NPC, Message)
+		
+	end
+}
+
+-------------------------------------------------------------------------------
+
+-- create the idle state
+
+-------------------------------------------------------------------------------
+Idle = {
+	onEnter = function(NPC)
+		NPC:StopMoving();
+		NPC:SetWaitDuration(5.0);
+	end,
+
+	Update = function(NPC)
+		if not NPC:IsWaiting() then 
+			FSM.ChangeState(NPC, "Wander");
+		end
+		if Detection.SeePlayer(NPC) then
+			Dispatch.SendMessage("PlayerSpotted", NPC, 3.0);
+			FSM.ChangeState(NPC, "Chase");
+		end	
+	end,
+
+	onExit = function(NPC)
+		NPC:ClearWaitDuration();
+	end,
+
+	onMessage = function(NPC, Message)
+		if Dispatch.InMessageRange(NPC, Message, 1000.0) then
+			if Message.Event == "PlayerSpotted" then
+				Movement.MoveTo(NPC, Dispatch.GetSenderLocation(Message))
+				FSM.ChangeState(NPC, "Investigate");
+			end
+		end
+	end
+}
+
+-------------------------------------------------------------------------------
+
+-- create the Wander state
+
+-------------------------------------------------------------------------------
+Wander = {
+	onEnter = function(NPC)
+		NPC:SetWander();
+		Movement.Wander(NPC);
+	end,
+
+	Update = function(NPC)
+		Movement.Wander(NPC);
+		--Movement.MoveTo(NPC, NPC:GetLastMoveTo());
+		--if not NPC:IsMoving() then
+		--	Movement.Wander(NPC);
+		--end
+		if Detection.SeePlayer(NPC) then
+			Dispatch.SendMessage("PlayerSpotted", NPC, 3.0);
+			FSM.ChangeState(NPC, "Chase");
+		end	
+	end,
+
+	onExit = function(NPC)
+		
+	end,
+
+	onMessage = function(NPC, Message)
+		if Dispatch.InMessageRange(NPC, Message, 1000.0) then
+			if Message.Event == "PlayerSpotted" then
+				Movement.MoveTo(NPC, Dispatch.GetSenderLocation(Message))
+				FSM.ChangeState(NPC, "Investigate");
+			end
+		end
+	end
+}
+
+-------------------------------------------------------------------------------
+
+-- create the chase state
+
+-------------------------------------------------------------------------------
+Chase = {
+	onEnter = function(NPC)
+		Movement.ChasePlayer(NPC);
+	end,
+
+	Update = function(NPC)
+		Movement.MoveTo(NPC, NPC:GetLastMoveTo());
+		if Detection.SeePlayer(NPC) then
+			Movement.ChasePlayer(NPC);
+			Dispatch.SendMessage("PlayerSpotted", NPC, 3.0);
+		elseif not Detection.SeePlayer(NPC) and not NPC:IsMoving() then
+			FSM.ChangeState(NPC, "Idle");
+		end
+	end,
+
+	onExit = function(NPC)
+
+	end
+}
+
+-------------------------------------------------------------------------------
+
+-- create the investigate state
+
+-------------------------------------------------------------------------------
+Investigate = {
+	onEnter = function(NPC)
+		
+	end,
+
+	Update = function(NPC)
+		Movement.MoveTo(NPC, NPC:GetLastMoveTo());
+		if Detection.SeePlayer(NPC) then
+			Dispatch.SendMessage("PlayerSpotted", NPC, 3.0);
+			FSM.ChangeState(NPC, "Chase");
+		elseif not Detection.SeePlayer(NPC) and not NPC:IsMoving() then
+			FSM.ChangeState(NPC, "Idle");
+		end
+	end,
+
+	onExit = function(NPC)
+
+	end,
+
+	onMessage = function(NPC, Message)
+		if Dispatch.InMessageRange(NPC, Message, 1000.0) then
+			if Message.Event == "PlayerSpotted" then
+				Movement.MoveTo(NPC, Dispatch.GetSenderLocation(Message))
+			end
+		end
+	end
+}
