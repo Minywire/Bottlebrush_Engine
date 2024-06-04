dd9030015bc9fa787934cdb4fd760dc193a8f22f
Jaiden di Lanzo <jaiden.dilanzo@bigpond.com>
2024-06-04 17:39:59 +0800

    Slight redesign of world creation mechanism + add some private typedefs and vars

--- Bottlebrush_Engine/Bottlebrush_Engine/BBPhysics/PhysicsMgr.cpp
+++ Bottlebrush_Engine/Bottlebrush_Engine/BBPhysics/PhysicsMgr.cpp
@@ -1,15 +1,13 @@
 #include "PhysicsMgr.h"
 
-PhysicsMgr::~PhysicsMgr() { DeleteWorld(); }
-
 PhysicsMgr& PhysicsMgr::GetInstance() {
     static PhysicsMgr instance_;
     return instance_;
 }
 
-uint32_t PhysicsMgr::CreatePhysicsBody(PhysicsBody::PhysicsBodyType type,
-                                       glm::vec3 position, glm::vec3 rotation,
-                                       glm::vec3 scale) {
+uint32_t PhysicsMgr::CreateBody(PhysicsBody::PhysicsBodyType type,
+                                glm::vec3 position, glm::vec3 rotation,
+                                glm::vec3 scale) {
     auto p = rp3d::Vector3(position.x, position.y, position.z);
     auto o = rp3d::Quaternion(rotation.x, rotation.y, rotation.z, 1.0f);
     auto t = rp3d::Transform(p, o);
@@ -28,21 +26,19 @@ uint32_t PhysicsMgr::CreatePhysicsBody(PhysicsBody::PhysicsBodyType type,
     return index;
 }
 
-const PhysicsBody& PhysicsMgr::ObtainPhysicsBody(uint32_t index) const {
-    return physics_bodies_.at(index);
-}
-
-void PhysicsMgr::RemovePhysicsBody(uint32_t index) {
-    // Destroy the rigid body in the RP3D physics world and erase it from the
-    // map to ensure we are cleaning up after ourselves nice and proper
-    world_->destroyRigidBody(physics_bodies_.at(index).GetRigidBody());
-    physics_bodies_.erase(index);
-}
-
-void PhysicsMgr::Update(float delta) { world_->update(delta); }
-
-PhysicsMgr::PhysicsMgr() { CreateWorld(); }
-
 void PhysicsMgr::CreateWorld() { world_ = common_.createPhysicsWorld(); }
 
 void PhysicsMgr::DeleteWorld() { common_.destroyPhysicsWorld(world_); }
+
+const PhysicsBody& PhysicsMgr::ObtainBody(uint32_t index) const {
+    return physics_bodies_.at(index);
+}
+
+uint32_t PhysicsMgr::RemoveBody(uint32_t index) {
+    // Destroy the rigid body in the RP3D physics world and erase it from the
+    // map to ensure we are cleaning up after ourselves nice and proper
+    world_->destroyRigidBody(physics_bodies_.at(index).GetRigidBody());
+    return physics_bodies_.erase(index);
+}
+
+void PhysicsMgr::Update(float delta) { world_->update(delta); }
--- Bottlebrush_Engine/Bottlebrush_Engine/BBPhysics/PhysicsMgr.h
+++ Bottlebrush_Engine/Bottlebrush_Engine/BBPhysics/PhysicsMgr.h
@@ -7,33 +7,37 @@
 #include "reactphysics3d/reactphysics3d.h"
 
 class PhysicsMgr {
-   public:
-    using PhysicsCommon = rp3d::PhysicsCommon;
+    using PhysicsCommn = rp3d::PhysicsCommon;
     using PhysicsWorld = rp3d::PhysicsWorld*;
 
-    ~PhysicsMgr();
+    inline static glm::vec3 kDefaultPosition = {0.0f, 0.0f, 0.0f};
+    inline static glm::vec3 kDefaultRotation = {0.0f, 0.0f, 0.0f};
+    inline static glm::vec3 kDefaultScale = {1.0f, 1.0f, 1.0f};
+    inline static PhysicsBody::PhysicsBodyType kDefaultType =
+        PhysicsBody::PhysicsBodyType::kStatic;
 
+   public:
     static PhysicsMgr& GetInstance();
 
-    uint32_t CreatePhysicsBody(PhysicsBody::PhysicsBodyType type =
-                                   PhysicsBody::PhysicsBodyType::kStatic,
-                               glm::vec3 position = {0.0f, 0.0f, 0.0f},
-                               glm::vec3 rotation = {0.0f, 0.0f, 0.0f},
-                               glm::vec3 scale = {1.0f, 1.0f, 1.0f});
-    const PhysicsBody& ObtainPhysicsBody(uint32_t index) const;
-    void RemovePhysicsBody(uint32_t index);
-
-    void Update(float delta);
-
-   private:
-    PhysicsMgr();
-
+    uint32_t CreateBody(PhysicsBody::PhysicsBodyType type = kDefaultType,
+                        glm::vec3 position = kDefaultPosition,
+                        glm::vec3 rotation = kDefaultRotation,
+                        glm::vec3 scale = kDefaultScale);
     void CreateWorld();
     void DeleteWorld();
+    const PhysicsBody& ObtainBody(uint32_t index) const;
+    uint32_t RemoveBody(uint32_t index);
+    void Update(float delta);
 
+    // TODO: DELETE THIS, IT IS FOR TESTING
+    rp3d::PhysicsWorld* GetWorld() { return world_; }
+
+   private:
+    PhysicsMgr() = default;
+
+    PhysicsCommn common_;
     std::unordered_map<uint32_t, PhysicsBody> physics_bodies_;
-    PhysicsCommon common_;
-    PhysicsWorld world_;
+    PhysicsWorld world_ = nullptr;
 };
 
 #endif  // BOTTLEBRUSH_ENGINE_PHYSICSMGR_H
