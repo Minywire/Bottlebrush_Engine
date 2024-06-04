48908bf589bd483c67b9a36ed4fcf5d0bc84f03d
Jaiden di Lanzo <jaiden.dilanzo@bigpond.com>
2024-06-04 17:50:43 +0800

    Add some default static vars

--- Bottlebrush_Engine/Bottlebrush_Engine/BBPhysics/PhysicsBody.h
+++ Bottlebrush_Engine/Bottlebrush_Engine/BBPhysics/PhysicsBody.h
@@ -5,15 +5,19 @@
 #include "reactphysics3d/reactphysics3d.h"
 
 class PhysicsBody {
+    inline static glm::vec3 kDefaultPosition = {0.0f, 0.0f, 0.0f};
+    inline static glm::vec3 kDefaultRotation = {0.0f, 0.0f, 0.0f};
+    inline static glm::vec3 kDefaultScale = {1.0f, 1.0f, 1.0f};
+
    public:
     using RigidBody = rp3d::RigidBody*;
 
     enum PhysicsBodyType { kStatic, kKinematic, kDynamic };
 
     explicit PhysicsBody(PhysicsBodyType type = kStatic,
-                         glm::vec3 position = {0.0f, 0.0f, 0.0f},
-                         glm::vec3 rotation = {0.0f, 0.0f, 0.0f},
-                         glm::vec3 scale = {1.0f, 1.0f, 1.0f});
+                         glm::vec3 position = kDefaultPosition,
+                         glm::vec3 rotation = kDefaultRotation,
+                         glm::vec3 scale = kDefaultScale);
 
     [[nodiscard]] const glm::vec3& GetPosition() const;
     [[nodiscard]] RigidBody GetRigidBody() const;
